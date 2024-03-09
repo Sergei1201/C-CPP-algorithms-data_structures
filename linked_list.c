599ad711d1767874caef3407a634f2fc8838734b
Sergei Sokolov <sokolov12ssa@gmail.com>
2024-03-09 18:45:36 +0500

    delete a node

--- linked_list.c
+++ linked_list.c
@@ -18,6 +18,9 @@ void insertAfter(struct Node *prevNode, int data);
 /* Insrt last element into the list*/
 void insertLast(struct Node **headRef, int data);
 
+/*Delete a node*/
+void deleteNode(struct Node **headRef, int key);
+
 /* Print the list*/
 void printList(struct Node *node);
 
@@ -29,6 +32,7 @@ int main()
     insertLast(&head, 70);
     insertLast(&head, 100);
     insertAfter(head->next, 90);
+    deleteNode(&head, 70);
     printList(head);
     return 0;
 }
@@ -74,6 +78,32 @@ void insertLast(struct Node **headRef, int data)
         temp = temp->next;
     }
     temp->next = newNode;
+}
+void deleteNode(struct Node **headRef, int key)
+{
+    struct Node *temp = *headRef, *prev;
+    /* Check the first node, if the key is present delete the node by moving the head to the next node */
+    if (temp != NULL && temp->data == key)
+    {
+        *headRef = temp->next;
+        free(temp);
+        return;
+    }
+    // Traverse the linked list
+    while (temp->next != NULL && temp->data != key)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+    // Check if there's no match
+    if (temp == NULL)
+    {
+        printf("No match has been found");
+        return;
+    }
+    // Delete a node
+    prev->next = temp->next;
+    free(temp);
 }
 void printList(struct Node *node)
 {
