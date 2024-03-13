86ab4497b509306f9356f727e738cd2d53b941bf
Sergei Sokolov <sokolov12ssa@gmail.com>
2024-03-13 23:34:47 +0500

    last commit for ll ds in C

--- linked_list/double_ll.c
+++ linked_list/double_ll.c
@@ -1,4 +1,3 @@
-/* Full implementation of a double linked list data structure in the C programming language */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,26 +20,26 @@ void insertAfter(struct Node *prevNode, int data);
 // Insert last
 void insertLast(struct Node **headRef, int data);
 
-// Print list
-void printList(struct Node *head);
-
 // Delete node
 void deleteNode(struct Node **headRef, struct Node *delNode);
 
+// Print list
+void printList(struct Node *head);
+
 int main()
 {
-    // Assume that we've got an empty list at the beginning
+    // At the beginning the list is empty
     struct Node *head = NULL;
+    insertFirst(&head, 20);
+    insertFirst(&head, 30);
     insertFirst(&head, 50);
-    insertFirst(&head, 90);
-    insertFirst(&head, 95);
-    insertLast(&head, 506);
-    insertLast(&head, 207);
-    insertAfter(head->next->next->next, 3300);
-    insertAfter(head->next->next->next->next->next, 1000);
-    deleteNode(&head, head->next->next->next->next);
-    deleteNode(&head, head->next->next->next->next->next);
+    insertLast(&head, 190);
+    insertLast(&head, 135);
+    insertFirst(&head, 295);
+    insertAfter(head, 350);
+    insertAfter(head->next->next, 999);
     deleteNode(&head, head);
+    deleteNode(&head, head->next->next);
     printList(head);
     return 0;
 }
@@ -52,11 +51,9 @@ void insertFirst(struct Node **headRef, int data)
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     // Assign values
     newNode->data = data;
-    // Point the next pointer to the head
     newNode->next = *headRef;
-    // Point the previous pointer to NULL since it's a newly created head of the list
     newNode->prev = NULL;
-    // Check if the head is empty, assign the new node to the head so that we can access that particular memory block, shift the head pointer to the new node
+    // Check if the head is empty, change the head to the new node
     if (*headRef == NULL)
     {
         newNode->prev = NULL;
@@ -72,86 +69,86 @@ void insertFirst(struct Node **headRef, int data)
 // Insert after
 void insertAfter(struct Node *prevNode, int data)
 {
-    // Check if there's the previous node in the list
+    // Check if there's no previous node
     if (!prevNode)
     {
-        printf("The previous node can't be null");
+        printf("The previous node cannot be null!");
         return;
     }
-    // Dynamically memory allocation on the heap for a new node
+    // Dynamic memory allocation on the heap for a new node
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     // Assign values
     newNode->data = data;
-    // Point the next pointer of the new node to the next node after the previous one
+    // Change the next pointer of the new node to the next pointer of the previous node
     newNode->next = prevNode->next;
-    // Point the next pointer of the previous node to the new node
+    // Change the next pointer of the previous node to the new node
     prevNode->next = newNode;
-    // Point the previous pointer of the new node to the previous one
+    // Change the previous pointer of the new node to the previous node
     newNode->prev = prevNode;
-    // Check if there is the next node after the new node, point the next node to the new node
-    if (newNode->next)
+    // Check if the next node is not null, change its previous pointer to the new node to rearrange the links
+    if (newNode->next != NULL)
     {
         newNode->next->prev = newNode;
     }
 }
 
-// Print last
+// Insert last
 void insertLast(struct Node **headRef, int data)
 {
-    // Dynamically allocate memory on the heap for a new node
+    // Dynamic memory allocation on the heap for a new node
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-    // Declare a temp variable to traverse the list
+    // Set a temp variable to traverse the list starting at the head
     struct Node *temp = *headRef;
     // Assign values
     newNode->data = data;
     newNode->next = NULL;
-    // Check if there's not head, set the new node to the head and return
-    if (*headRef == NULL)
+    // Check if there's not head, point the new node to the head
+    if (!*headRef)
     {
         newNode->prev = NULL;
         *headRef = newNode;
     }
-    // Traverse the list and point the last node to the new node in both directions
+    // Traverse the list, point the last node to the new node, point the new node to the previous one, thus rebuilding links
     while (temp->next)
     {
         temp = temp->next;
     }
     temp->next = newNode;
-    // Change pointers to point to the previous node
     newNode->prev = temp;
 }
 
 // Delete node
 void deleteNode(struct Node **headRef, struct Node *delNode)
 {
-    // If there's no head or not delNode, return
+    // If there's no head or delNode, return
     if (*headRef == NULL || delNode == NULL)
     {
-        printf("No node to be deleted");
+        printf("There's no node to be deleted");
         return;
     }
-    // Check if the delNode is in the head, move the next pointer of the head to the next node
+    // Check if the delNode is in the head, move the head to the next node
     if (*headRef == delNode)
     {
         *headRef = delNode->next;
     }
-    // Check if the delNode is node the last node, change the previous pointer of the next node to the previous pointer of the delNode
-    if (delNode->next != NULL)
+    // Check if the delNode is not the last node, change the previous pointer of the next node to the previous pointer of the delNode
+    if (delNode->next)
     {
         delNode->next->prev = delNode->prev;
     }
-    // Check if the delNode is not in the head, change the next pointer of the previous node to the next pointer of the delNode
-    if (delNode->prev != NULL)
+    // Check if the delNode is not the first node (head), change the next pointer of the previous node to the next pointer of the delNode
+    if (delNode->prev)
     {
         delNode->prev->next = delNode->next;
     }
-    // Deallocate memory for delNode after completion
+    // When done, deallocate memory for delNode
     free(delNode);
 }
 
+// Print list
 void printList(struct Node *head)
 {
-    // Set a temp value to traverse the linked list
+    // Traverse the list and print data of the nodes on each iteration
     struct Node *temp = head;
     while (temp)
     {
