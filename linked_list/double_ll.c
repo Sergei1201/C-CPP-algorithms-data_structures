552139466c61fdef549e22558784b11f1a40f430
Sergei Sokolov <sokolov12ssa@gmail.com>
2024-03-13 22:50:36 +0500

    full implementation of ll in C

--- linked_list/double_ll.c
+++ linked_list/double_ll.c
@@ -1,3 +1,4 @@
+/* Full implementation of a double linked list data structure in the C programming language */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,11 +15,17 @@ struct Node
 // Insert first
 void insertFirst(struct Node **headRef, int data);
 
+// Insert after
+void insertAfter(struct Node *prevNode, int data);
+
 // Insert last
 void insertLast(struct Node **headRef, int data);
 
 // Print list
 void printList(struct Node *head);
+
+// Delete node
+void deleteNode(struct Node **headRef, struct Node *delNode);
 
 int main()
 {
@@ -29,6 +36,11 @@ int main()
     insertFirst(&head, 95);
     insertLast(&head, 506);
     insertLast(&head, 207);
+    insertAfter(head->next->next->next, 3300);
+    insertAfter(head->next->next->next->next->next, 1000);
+    deleteNode(&head, head->next->next->next->next);
+    deleteNode(&head, head->next->next->next->next->next);
+    deleteNode(&head, head);
     printList(head);
     return 0;
 }
@@ -57,8 +69,34 @@ void insertFirst(struct Node **headRef, int data)
     }
 }
 
+// Insert after
+void insertAfter(struct Node *prevNode, int data)
+{
+    // Check if there's the previous node in the list
+    if (!prevNode)
+    {
+        printf("The previous node can't be null");
+        return;
+    }
+    // Dynamically memory allocation on the heap for a new node
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    // Assign values
+    newNode->data = data;
+    // Point the next pointer of the new node to the next node after the previous one
+    newNode->next = prevNode->next;
+    // Point the next pointer of the previous node to the new node
+    prevNode->next = newNode;
+    // Point the previous pointer of the new node to the previous one
+    newNode->prev = prevNode;
+    // Check if there is the next node after the new node, point the next node to the new node
+    if (newNode->next)
+    {
+        newNode->next->prev = newNode;
+    }
+}
+
 // Print last
-void printLast(struct Node **headRef, int data)
+void insertLast(struct Node **headRef, int data)
 {
     // Dynamically allocate memory on the heap for a new node
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
@@ -81,6 +119,34 @@ void printLast(struct Node **headRef, int data)
     temp->next = newNode;
     // Change pointers to point to the previous node
     newNode->prev = temp;
+}
+
+// Delete node
+void deleteNode(struct Node **headRef, struct Node *delNode)
+{
+    // If there's no head or not delNode, return
+    if (*headRef == NULL || delNode == NULL)
+    {
+        printf("No node to be deleted");
+        return;
+    }
+    // Check if the delNode is in the head, move the next pointer of the head to the next node
+    if (*headRef == delNode)
+    {
+        *headRef = delNode->next;
+    }
+    // Check if the delNode is node the last node, change the previous pointer of the next node to the previous pointer of the delNode
+    if (delNode->next != NULL)
+    {
+        delNode->next->prev = delNode->prev;
+    }
+    // Check if the delNode is not in the head, change the next pointer of the previous node to the next pointer of the delNode
+    if (delNode->prev != NULL)
+    {
+        delNode->prev->next = delNode->next;
+    }
+    // Deallocate memory for delNode after completion
+    free(delNode);
 }
 
 void printList(struct Node *head)
