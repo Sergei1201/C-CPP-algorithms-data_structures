5aa9f3a54483637b0534e53705f614c13b08b909
Sergei Sokolov <sokolov12ssa@gmail.com>
2024-03-14 23:23:57 +0500

    double ll in c++

--- linked_list/double_ll.cpp
+++ linked_list/double_ll.cpp
@@ -1,4 +1,4 @@
-/*Full implementation of double linked list in the C++ programming language in the object-oriented fashion*/
+/* Full implementation of a double linked list with C++ in the object-oriented-fashion */
 #include <iostream>
 
 /* Node class */
@@ -8,14 +8,8 @@ public:
     int m_data;
     Node *next;
     Node *prev;
-
-public:
     // Default constructor
-    Node()
-        : m_data(0),
-          next(NULL), prev(NULL)
-    {
-    }
+    Node() = default;
     // Parameterized constructor
     Node(int data)
         : m_data(data), next(NULL), prev(NULL)
@@ -28,13 +22,12 @@ class LinkedList
 {
 public:
     Node *head;
-
-public:
     // Default constructor
     LinkedList()
         : head(NULL)
     {
     }
+
     /* Public member functions */
 
     // Insert first
@@ -47,21 +40,24 @@ public:
     void insertLast(int data);
 
     // Delete node
-    void deleteNode(Node *delData);
+    void deleteNode(Node *delNode);
 
-    // Print list
+    // Print linked list
     void printList();
 };
 
 void LinkedList::insertFirst(int data)
 {
-    // Dynamic memory allocation for a new node on the heap
-    Node *newNode = new Node(data);
-    // Point the next pointer to the head
+    // Dynamic memory allocation on the heap for a new node
+    Node *newNode = new Node();
+    // Assign values to the allocated memory (type of Node)
+    newNode->m_data = data;
+    // Change the next pointer of the new node to the head
     newNode->next = head;
+    // Change the previous pointer of the new node to NULL (make it the head node)
     newNode->prev = NULL;
-    // Check if the head is empty, set the new node to the head and shift the pointer of the head to it
-    if (head == NULL)
+    // Check if the head is NULL
+    if (!head)
     {
         head = newNode;
         newNode->prev = NULL;
@@ -75,112 +71,107 @@ void LinkedList::insertFirst(int data)
 
 void LinkedList::insertAfter(Node *prevNode, int data)
 {
-    // Check if the previous node is null
-    if (prevNode == NULL)
+    // Check if there's no previous node
+    if (!prevNode)
     {
         std::cout << "The previous node cannot be null!";
         return;
     }
-    // Dynamic memory allocation for a new node on the heap
+    // Dynamically memory allocation on the heap for a new node
     Node *newNode = new Node();
     // Assign values
     newNode->m_data = data;
-    // Point the next pointer to the node that is next to the previous pointer
+    // Change the next link of the new node to the next link of the previous node
     newNode->next = prevNode->next;
-    // Point the previous pointer to the previous node
+    // Change the previous link of the new node to the previous node
     newNode->prev = prevNode;
-    // Point the next pointer of the previous node to the new node
+    // Change the link of the next node of the previous node to the new node
     prevNode->next = newNode;
-    // If the new node is not the last one, change the next pointer to the new node
-    if (prevNode->next)
+    // Check if the new node is not the last node in the list, change the previous pointer of the next node to the new node (to readjust links)
+    if (newNode->next)
     {
-        prevNode->next->prev = newNode;
+        newNode->next->prev = newNode;
     }
+}
+
+void LinkedList::deleteNode(Node *delNode)
+{
+    // Check if there's no head or no delNode
+    if (!head || !delNode)
+    {
+        std::cout << "No data to be deleted!";
+        return;
+    }
+    // Check if the delNode is in the head, shift the head node to the next node
+    if (head == delNode)
+    {
+        head = delNode->next;
+    }
+    // Check if the delNode is not the last node in the linked list
+    // Point the previous pointer of the next node to the previous pointer of the current node
+    if (delNode->next)
+    {
+        delNode->next->prev = delNode->prev;
+    }
+    // Check if the delNode is not in the head
+    if (delNode->prev)
+    {
+        delNode->prev->next = delNode->next;
+    }
+    // Free memory for delNode pointer
+    delete delNode;
 }
 
 void LinkedList::insertLast(int data)
 {
-    // Dynamic memory allocation for a new node on the heap
+    // Dynamically memory allocation on the heap for a new node
     Node *newNode = new Node();
-    // Set temp variable to traverse the linked list from the head
+    // Set temp variable to traverse the linked list
     Node *temp = head;
-    // Assign values to the memory block - dereferene pointers
+    // Assign values
     newNode->m_data = data;
+    // Point the next pointer of the new node to NULL since it's going to be the last node
     newNode->next = NULL;
-    // Check if the head is NULL, set the new node to the head
-    if (head == NULL)
+    // Check if the head points to null, change the head to the new node and return
+    if (!head)
     {
         head = newNode;
         newNode->prev = NULL;
         return;
     }
-    // Traverse the list till the end and point the last node to the new node
+    // Traverse the linked list from the head, point the last node to the new node, point the new node to the previous node
     while (temp->next)
     {
         temp = temp->next;
     }
     temp->next = newNode;
-    // Change the previous pointer of the new node to the previous node to readjust the links
-    newNode = temp;
-}
-
-void LinkedList::deleteNode(Node *delNode)
-{
-    // If there's no head or no node to be deleted
-    if (!head || !delNode)
-    {
-        std::cout << "There's no node to be deleted from the list!";
-        return;
-    }
-    // Check if the key is in the head, shift the head to the next node
-    if (head == delNode)
-    {
-        head = delNode->next;
-    }
-    // Check if the node is not the last one, change the previous pointer of the next node to the previous ponter of the current node
-    if (delNode->next)
-    {
-        delNode->next->prev = delNode->prev;
-    }
-    // Check if the delNode is not in the head, change the next pointer of the previous node to the next pointer of the delNode
-    if (delNode->prev)
-    {
-        delNode->prev->next = delNode->next;
-    }
-    // Deallocate memory for the delNode on the heap
-    delete delNode;
+    newNode->prev = temp;
 }
 
 void LinkedList::printList()
 {
+    // Traverse the linked list till the end and print values of each node
+    Node *temp = head;
+    while (temp)
     {
-        // Traverse the list and print the value of nodes on each iteration
-        Node *temp = head;
-        while (temp)
-        {
-            std::cout << "Double linked list: " << temp->m_data << " ";
-            temp = temp->next;
-        }
-        std::cout << std::endl;
+        std::cout << "Linked list: " << temp->m_data << " ";
+        temp = temp->next;
     }
+    std::cout << std::endl;
 }
 
 int main()
 {
-    // Instantiate a linked list class by creating an object
+    // Instantiate the linked list class
     LinkedList ll;
-    ll.insertFirst(20);
+    ll.insertFirst(25);
     ll.insertFirst(30);
-    ll.insertFirst(55);
-    ll.insertLast(100);
-    ll.insertLast(200);
-    ll.insertAfter(ll.head->next, 260);
-    ll.insertAfter(ll.head->next->next, 350);
-    ll.insertAfter(ll.head, 956);
-    ll.insertAfter(ll.head->next->next->next->next->next, 1001);
-    ll.insertLast(2002);
-    ll.insertLast(20055);
-    ll.deleteNode(ll.head->next);
+    ll.insertFirst(40);
+    ll.insertAfter(ll.head->next, 100);
+    ll.insertLast(192);
+    ll.insertLast(-192);
+    ll.insertAfter(ll.head->next->next->next->next, 302);
+    ll.insertFirst(35);
     ll.printList();
     return 0;
 }
