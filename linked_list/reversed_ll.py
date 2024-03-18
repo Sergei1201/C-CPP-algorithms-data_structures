a2d3862574b8e56f051f14e164c3bfb0063e9bae
Sergei Sokolov <sokolov12ssa@gmail.com>
2024-03-18 20:42:04 +0500

    ll reverse in c/c++ and python

--- linked_list/reversed_ll.py
+++ linked_list/reversed_ll.py
@@ -1,4 +1,4 @@
-# Full implementation of a reversed linked list data structure in the Python programming language
+# Full implementation of the reversed linked list data structure using iteration in the Python programming language
 
 # Node class
 class Node:
@@ -14,38 +14,42 @@ class LinkedList:
     # Constructor
     def __init__(self):
         self.head = None
-
     # Methods
 
-    # Push a new node to the linked list
+    # Push a new node to the linked list (at the beginning of the linked list)
     def push_node(self, data):
         # Create a new node
         new_node = Node(data)
-        # Assign values
+        # Change the next pointer of the new node to the head pointer
         new_node.next = self.head
-        # Change the head to the new node
+        # Shift the head pointer to the new node thus making it the head node
         self.head = new_node
 
-    # Reverse linked list
+    # Reverse the linked list
     def reverse_list(self):
+        # Consider the case where there's no head or no next node. In this case there's nothing to reverse, just return and that's it
+        if (not self.head or not self.head.next):
+            return
         # Create some pointers to traverse the linked list
         current = self.head
         next = None
         prev = None
-        # Traverse the linked list, reverse pointers on each iteration and finally shift the head to the last node of the linked list
+        # Traverse the linked list changing its pointers to the opposite direction using iteration
         while (current):
             next = current.next
             current.next = prev
             prev = current
             current = next
+        # Shift the head pointer to the last node (prev)
         self.head = prev
 
-    # Print linked list
+    # Print the linked list
     def print_list(self):
-        # Set a temporary variable to traverse the linked list and print values of all nodes on each iteration starting from the head
+        # Assign a temp variable to the head to start traversing the linked list
         temp = self.head
+        # Start traversing the linked list printing values of every node on each iteration
         while (temp):
-            print(f'Linked list {temp.data} ', end='')
+            print(f'Linked list: {temp.data} ', end='')
             temp = temp.next
         print()
 
@@ -53,11 +57,15 @@ class LinkedList:
 if __name__ == '__main__':
     # Instantiate the linked list class
     ll = LinkedList()
+    # Push new nodes to the linked list
+    ll.push_node(35)
+    ll.push_node(45)
     ll.push_node(50)
-    ll.push_node(60)
+    ll.push_node(65)
     ll.push_node(70)
-    ll.push_node(80)
-    ll.push_node(90)
+    # Print the linked list after inserting nodes in it
     ll.print_list()
+    # Reverse the linked list
     ll.reverse_list()
+    # Print the linked list after reversing
     ll.print_list()
