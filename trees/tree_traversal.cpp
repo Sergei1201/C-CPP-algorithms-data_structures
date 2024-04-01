0f33e66c6bd2edc1c486e973cadd1154cf4ed4e7
Sergei Sokolov <sokolov12ssa@gmail.com>
2024-04-01 21:09:22 +0500

    DSA tree traversal in C++

--- trees/tree_traversal.cpp
+++ trees/tree_traversal.cpp
@@ -1,104 +1,89 @@
-/* Tree traversal DSA implementation in C++ */
+/* Tree traversal DSA implementation in C++ using recursion */
 #include <iostream>
-
 /* Node class */
 class Node
 {
-    // Private data members
 public:
     int m_data;
-    Node *left;
-    Node *right;
-    /* Public constructors */
-public:
-    // Default constructor
-    Node() = default;
-    // Parameterized constructor
+    Node *left, *right;
+    // Constructor
     Node(int data)
         : m_data(data), left(NULL), right(NULL)
     {
     }
-
     /* Public member functions */
 
-    // Preorder traversal (Root - > Left subtree - > Right subtree)
-    void preorderTraversal(Node *root)
-    {
-        // Check if the root is null, else traverse recursively
-        if (!root)
-        {
-            return;
-        }
-        std::cout << "The tree: " << root->m_data << " ";
-        preorderTraversal(root->left);
-        preorderTraversal(root->right);
-    }
-
-    // Inorder traversal (Left subtree - > Root - > Right subtree)
+    // Inorder traversal of the tree
     void inorderTraversal(Node *root)
     {
-        // Check if the root is null, else traverse recursively
+        // Check if the root is null
         if (!root)
         {
             return;
         }
+        // otherwise implement recursion to traverse the tree inorder
+        // Traverse the left subtree recursively
         inorderTraversal(root->left);
+        // Visit the root node
         std::cout << "The tree: " << root->m_data << " ";
         inorderTraversal(root->right);
     }
 
-    // Postorder traversal (Left subtree - > Right subtree - > Root)
-    void postorderTraversal(Node *root)
+    // Preorder traversal of the tree
+    void preorderTraversal(Node *root)
     {
-        // Check if the root is null, otherwise traverse the tree recursively
+        // Check if the root is null
         if (!root)
         {
             return;
         }
-        postorderTraversal(root->left);
-        postorderTraversal(root->right);
+        // otherwise implement recursion to traverse the tree preorderly
+        // Visit the root node
         std::cout << "The tree: " << root->m_data << " ";
+        // Walk the left subtree recursively
+        preorderTraversal(root->left);
+        // Walk the right subtree recursively
+        preorderTraversal(root->right);
     }
 
-    // Create a new node
-    Node *createNewNode(int data)
+    // Postorder traversal of the tree
+    void postorderTraversal(Node *root)
     {
-        // Dynamic memory allocation on the heap for a new node
-        Node *newNode = new Node;
-        // Assign values
-        newNode->m_data = data;
-        newNode->left = NULL;
-        newNode->right = NULL;
-        return newNode;
-    }
-
-    // Insert a new node to the left subtree
-    void insertLeft(Node *root, int data)
-    {
-        root->left = createNewNode(data);
-    }
-
-    // Insert a new node to the right subtree
-    void insertRight(Node *root, int data)
-    {
-        root->right = createNewNode(data);
+        // Check if the root is null
+        if (!root)
+        {
+            return;
+        }
+        // otherwise implement recursion to traverse the tree postorderly
+        // Walk the left subtree recursively
+        postorderTraversal(root->left);
+        // Walk the right subtree recursively
+        postorderTraversal(root->right);
+        // Visit the root node
+        std::cout << "The tree: " << root->m_data << " ";
     }
 };
 
 int main()
 {
-    // Instantiate the Node class
-    Node root{};
-    Node *newRoot = root.createNewNode(60);
-    root.insertLeft(newRoot, 55);
-    root.insertRight(newRoot, 22);
-    root.insertLeft(newRoot->left, 35);
-    root.insertLeft(newRoot->left->left, 66);
-    root.insertRight(newRoot->right, 100);
-    root.inorderTraversal(newRoot);
+    // Dynamically allocate memory on the heap for new nodes of our tree
+    Node *root = new Node(5);
+    root->left = new Node(7);
+    root->left->left = new Node(8);
+    root->left->right = new Node(20);
+    root->left->left->left = new Node(10);
+    root->left->left->right = new Node(15);
+    root->right = new Node(30);
+    root->right->left = new Node(50);
+    root->right->right = new Node(65);
+    root->right->right->left = new Node(100);
+    root->right->right->right = new Node(90);
+    root->inorderTraversal(root);
     std::cout << std::endl;
-    root.preorderTraversal(newRoot);
+    root->preorderTraversal(root);
     std::cout << std::endl;
-    root.postorderTraversal(newRoot);
+    root->postorderTraversal(root);
     std::cout << std::endl;
+    // Deallocate the memory on the heap after using it
+    delete root;
 }
